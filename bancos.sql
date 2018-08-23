8c0b60bc38c2046458d14d2b1d668087b91e61ae
lisazevedo <lisazevedo3@gmail.com>
2018-08-22 23:12:05 -0300

    .sql dos bancos

---
+++ bancos.sql
@@ -0,0 +1,14 @@
+﻿SET IDENTITY_INSERT [dbo].[Pubs] ON
+INSERT INTO [dbo].[Pubs] ([ID], [Name], [Address], [Rate]) VALUES (1, N'San Sebastian Salvador', N'Rua Conselheiro Pedro Luiz, 113 - Rio Vermelho, Salvador', 3.7)
+INSERT INTO [dbo].[Pubs] ([ID], [Name], [Address], [Rate]) VALUES (2, N'30 Segundos', N'Rua Ilhéus, 21 - Rio Vermelho, Salvador', 4)
+INSERT INTO [dbo].[Pubs] ([ID], [Name], [Address], [Rate]) VALUES (3, N'Confraria do França', N'Travessa Lydio de Mesquita, 43 - Rio Vermelho', 4.5)
+INSERT INTO [dbo].[Pubs] ([ID], [Name], [Address], [Rate]) VALUES (4, N'Chupito', N'Rua da Paciência, 263 - Rio Vermelho, Salvador', 4.3)
+INSERT INTO [dbo].[Pubs] ([ID], [Name], [Address], [Rate]) VALUES (5, N'116 Music Bar', N'Rua da Paciência, 116 - Rio Vermelho, Salvador', 3.9)
+INSERT INTO [dbo].[Pubs] ([ID], [Name], [Address], [Rate]) VALUES (6, N'Lebowski Pub', N'Rua da Paciência, 127 - Rio Vermelho, Salvador', 4.4)
+INSERT INTO [dbo].[Pubs] ([ID], [Name], [Address], [Rate]) VALUES (7, N'Passarela Bar', N'Rua João Gomes, 4 - Rio Vermelho, Salvador', 5)
+INSERT INTO [dbo].[Pubs] ([ID], [Name], [Address], [Rate]) VALUES (8, N'Amnésia Pub', N'Rua João Gomes, 63 - Rio Vermelho, Salvador', 4.6)
+INSERT INTO [dbo].[Pubs] ([ID], [Name], [Address], [Rate]) VALUES (9, N'RED Depósito Bar', N'Rua da Paciência, 233 - Rio Vermelho, Salvador', 4.7)
+INSERT INTO [dbo].[Pubs] ([ID], [Name], [Address], [Rate]) VALUES (10, N'Largo De Santana', N'Rio Vermelho, Salvador', 0)
+INSERT INTO [dbo].[Pubs] ([ID], [Name], [Address], [Rate]) VALUES (11, N'Boteco RV 24hs', N'Largo de Santana, 4 - Rio Vermelho, Salvador', 4)
+INSERT INTO [dbo].[Pubs] ([ID], [Name], [Address], [Rate]) VALUES (12, N'Bar Ana Cláudia', N'Estrada do Curralinho - Imbuí - Salvador', 0)
+SET IDENTITY_INSERT [dbo].[Pubs] OFF
