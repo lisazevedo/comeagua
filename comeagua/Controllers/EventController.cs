66e4ccb732a09af7c8b0d5f9fce624059961cbe2
lisazevedo <lisazevedo3@gmail.com>
2018-08-23 01:11:59 -0300

    ^^

--- comeagua/Controllers/EventController.cs
+++ comeagua/Controllers/EventController.cs
@@ -11,20 +11,27 @@ namespace comeagua.Controllers
 {
     public class EventController : Controller
     {
-        public ActionResult CreateEvent(Event evento)
+        public ActionResult CreateEvent(CreateEventViewModel model, string code)
         {
             var db = new ApplicationDbContext();
             db.Start();
 
-            var pub = db.Pubs.Single(p => p.ID == evento.PubID);
+            var pub = db.Pubs.Where(p => p.Name == model.BarName).FirstOrDefault();
 
-            if(pub != null)
+            if (pub != null)
             {
-                db.Events.Add(evento);
-                db.SaveChanges();
+                if (User.Identity.IsAuthenticated)
+                {
+                    var maneger = new ApplicationUserManager(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+                    var MyUser = maneger.Users.Where(x => x.UserName == HttpContext.User.Identity.Name).FirstOrDefault();
+
+                    var evento = new Event { PubID = pub.ID, Hour = model.Hour, Date = model.DateEvent, Code = code, AspNetUserID = MyUser.Id };
+                    db.Events.Add(evento);
+                    db.SaveChanges(); //usuario 
+                }
             }
-            
-            return View(); 
+
+            return View();
         }
 
         public ActionResult FindEvent(string code)
