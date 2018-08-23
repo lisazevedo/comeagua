306466f39cc905ff2e54afd4b3687b5a29896a48
caio mendes mendes <caiosmendes@hotmail.com>
2018-08-23 01:05:31 -0300

    Event

--- comeagua/Controllers/HomeController.cs
+++ comeagua/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using comeagua.Models;
+﻿using comeagua.Infra.Tables;
+using comeagua.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
@@ -32,15 +33,64 @@ namespace comeagua.Controllers
             //Session["UserLOGIN"] = "Lis";
             return View();
         }
-        public ActionResult Evento(String nome_bar,String data,String time)
+        public ActionResult CreateEvent(Event evento)
+        {
+            var db = new ApplicationDbContext();
+            db.Start();
+
+            var pub = db.Pubs.Single(p => p.ID == evento.PubID);
+
+            if (pub != null)
+            {
+                db.Events.Add(evento);
+                db.SaveChanges();
+            }
+
+            return View();
+        }
+
+        public ActionResult FindEvent(string code)
+        {
+            var db = new ApplicationDbContext();
+
+            db.Start();
+            var evento = db.Events.Single(e => e.Code.Equals(code));
+
+            if (User.Identity.IsAuthenticated && evento != null)
+            {
+                var maneger = new ApplicationUserManager(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+                var MyUser = maneger.Users.Where(x => x.UserName == HttpContext.User.Identity.Name).FirstOrDefault();
+
+                evento.AspNetUsers.Add(MyUser);
+                db.SaveChanges(); //usuario 
+            }
+
+            return View(); //evento nao existe ou user nao logado
+        }
+        public ActionResult Evento(String nome_bar, String data, String time)
         {
             if (User.Identity.IsAuthenticated)
             {
                 var maneger = new ApplicationUserManager(new UserStore<ApplicationUser>(new ApplicationDbContext()));
                 var MyUser = maneger.Users.Where(x => x.UserName == HttpContext.User.Identity.Name).FirstOrDefault();
                 ViewBag.ImageProfile = MyUser.Image;
+
+                var codigos = new List<int>();
+                Random random = new Random();
+                var codigoaleatorio = random.Next(0, 1000);
+                if (codigos.Contains(codigoaleatorio))
+                {
+                    codigoaleatorio = random.Next(0, 1000);
+                    ViewBag.CodigoEvento = codigoaleatorio;
+
+                }
+                else
+                {
+                    ViewBag.CodigoEvento = codigoaleatorio;
+                }
             }
             return View();
         }
+      
     }
 }
